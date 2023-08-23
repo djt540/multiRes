cb0b3c2603e2ac39f4d249009c29bee860312f32
djt540 <78608166+djt540@users.noreply.github.com>
2023-08-23 10:15:50 +0100

    make rounded

--- util/DataAnalysis.py
+++ util/DataAnalysis.py
@@ -4,11 +4,13 @@ import seaborn as sns
 import pandas as pd
 
 if __name__ == "__main__":
-    data = np.loadtxt('../test-results.csv', delimiter=',')
+    data = np.loadtxt('test-results.csv', delimiter=',')
 
-    steps = 10
-    cols = [0.1 + (i/steps) for i in range(steps)]
-    inds = [0.1 + (i/steps) for i in range(steps)]
+    steps = 15
+    cols = [round(0.1 + (i/steps), 2) for i in range(steps)]
+    inds = [round(0.1 + (i/steps), 2) for i in range(steps)]
+
+    print(cols)
 
     df = pd.DataFrame(data, columns=cols, index=inds)
 
